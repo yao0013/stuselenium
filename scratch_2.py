1cfc85abf7c262af6a79d2d33c0f9d891f415222
Administrator <yao0013@126.com>
2019-05-17 23:12:37 +0800

    the one

---
+++ scratch_2.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+driver = webdriver.Chrome()
+driver.get("http://www.baidu.com")
+print("这是设置的高度800*800")
+driver.set_window_size(800,800)
+driver.find_element_by_id("kw").send_keys("selenium2")
+driver.find_element_by_id("su").click()
+driver.refresh()
+driver.back()
+driver.find_element_by_id("kw").send_keys("xiaozemaliya")
+driver.find_element_by_id("su").submit()
+driver.find_element_by_id("kw").clear()
+right_click = driver.find_element_by_id("su")
+ActionChains(driver).context_click(right_click).perform()
