01f5ae6bcd3b4d1027a629da9207288d4348b0b3
Administrator <yao0013@126.com>
2019-06-03 22:44:53 +0800

    添加类和引用类 add类

---
+++ test.py
@@ -0,0 +1,17 @@
+from calculator import Count
+
+class TestCount:
+
+    def test_add(self):
+        try:
+            j = Count(2,3)
+            add = j.add()
+            assert (add == 5),'Intreger addition result error!'
+        except AssertionError as msg:
+            print(msg)
+        else:
+            print('Test pass!')
+
+
+mytest=TestCount()
+mytest.test_add()
