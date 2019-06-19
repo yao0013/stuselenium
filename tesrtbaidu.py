6ed9c64f697b87cae2131e300ebae367020093f0
yao0013 <yao0013@126.com>
2019-06-19 22:49:47 +0800

    html报告

---
+++ tesrtbaidu.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+import unittest
+from HTMLTestRunner import HTMLTestRunner
+
+class Badidu(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(10)
+        self.base_url = "http://www.baidu.com"
+
+    def test_Baidu_search(self):
+        driver = self.driver
+        driver.get(self.base_url)
+        driver.find_element_by_id("kw").send_keys("HTMLTestRunner")
+        driver.find_element_by_id("su").click()
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+
+    testunit = unittest.TestSuite()
+    testunit.addTest(Badidu("test_baidu_search"))
+
+    fp = open('./result.html','wb')
+
+    runner = HTMLTestRunner(stream=fp,
+                            title='百度搜索报告',
+                            description='用例执行情况：')
+    runner.run(testunit)
+    fp.close()
