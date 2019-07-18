243c1e9e81b763d71b4bf34d85d08af5ad4fa1d3
yao0013 <yao0013@126.com>
2019-07-18 22:53:16 +0800

    爬虫2

--- qiushibaikeSCA.py
+++ qiushibaikeSCA.py
@@ -9,5 +9,26 @@ html = resq.text
 
 names = re.findall('<h2>(.*?)</h2>',html,re.S)
 
-for name in names:
-    print(name.strip())
+contents = re.findall('<span>.\n(.*?)</span>',html,re.S)
+
+fms = re.findall('div class="articleGender(.*?)Icon"',html,re.S)
+
+ages = re.findall('div class="articleGender.*?Icon">(.*?)</div',html,re.S)
+
+laughs = re.findall('<i class="number">(.*?)</i>',html,re.S)
+
+
+#for name in names:
+ #   print(name.strip())
+
+for content in contents:
+    print(content)
+
+for fm in fms:
+    print(fm)
+
+for age in ages:
+     print(age)
+
+for laugh in laughs:
+     print(laugh)
